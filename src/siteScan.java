3fbd96de2b102ac4dbfd4440217df6ae30f19f18
Renan Almeida <renan.almeida@worldline.com>
2019-11-08 15:52:48 +0100

    Starting to read the website

--- src/siteScan.java
+++ src/siteScan.java
@@ -1,17 +1,30 @@
 import java.io.*;
+import java.net.URL;
 
 public class siteScan {
     /* Objective: Scan a website and do something with the content of the website
-     *  URL: https://wahlinfastigheter.se/
+     *  URL: https://wahlinfastigheter.se/lediga-objekt/forrad/
      * */
 
-
     public static void main(String[] args) {
-        File file = new File("example.txt");
+        File file = null;
         BufferedReader br;
         String line;
-
+        BufferedWriter writer;
+        int count=1;
         try {
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(
+                            new URL("https://wahlinfastigheter.se/lediga-objekt/forrad/").openStream()));
+            writer = new BufferedWriter(new FileWriter("siteHTML.txt"));
+            writer.write("Testing");
+
+            while ( (line = in.readLine()) != null) {
+                System.out.println((count+=1));
+                //System.out.println(line);
+            }
+
+            file = new File("siteHTML.txt");
             FileReader fr = new FileReader(file);
             br = new BufferedReader(fr);
             while ((line = br.readLine()) != null) {
@@ -19,26 +32,11 @@ public class siteScan {
             }
             br.close();
         } catch (FileNotFoundException e) {
+            assert file != null;
             System.out.println("File not found: " + file.toString());
         } catch (IOException e) {
+            assert file != null;
             System.out.println("Unable to read file: " + file.toString());
         }
-
-
-        Camera cam1 = new Camera();
-        try {
-            cam1.setId(5);
-            cam1.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-
-        Car car1 = new Car();
-        car1.setId(4);
-
-//        Machine machine1 = new Machine();
-        car1.run();
-
     }
 }
